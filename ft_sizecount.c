a9b33d21f05b4ff1a3b16b1d16b163ac9114638d
Saro Park <sapark@e1z2r6p8.42.us.org>
2019-05-17 05:03:05 -0700

    .

--- ft_sizecount.c
+++ ft_sizecount.c
@@ -6,7 +6,7 @@
 /*   By: sapark <sapark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/16 17:17:16 by sapark            #+#    #+#             */
-/*   Updated: 2019/05/16 17:21:23 by sapark           ###   ########.fr       */
+/*   Updated: 2019/05/17 03:29:44 by sapark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,19 @@
 
 size_t	ft_sizecount(char const *s, char c)
 {
-	unsigned int	len;
-	unsigned int	cnt;
+	int	i;
+	int	cnt;
 
-	cnt = 0;
-	len = ft_strlen(s);
-	while (len > 0)
+	i = 0;
+	if (s[0] != c)
+		cnt = 1;
+	else
+		cnt = 0;
+	while (s[i] && s[i + 1])
 	{
-		if (*s == c)
+		if (s[i] == c && s[i + 1] != c)
 			cnt++;
-		len--;
+		i++;
 	}
-	return (len - cnt);
+	return ((size_t)cnt);
 }
--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: sapark <sapark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 00:46:29 by sapark            #+#    #+#             */
-/*   Updated: 2019/05/16 23:07:21 by sapark           ###   ########.fr       */
+/*   Updated: 2019/05/17 04:46:16 by sapark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,76 +15,27 @@
 char	**ft_strsplit(char const *s, char c)
 {
 	char			**res;
-	int				i;
+	unsigned int	i;
 	int				j;
-	unsigned int	size;
 	unsigned int	start;
 
 	if (!s)
 		return (NULL);
 	i = 0;
 	j = 0;
-	size = ft_sizecount(s, c);
 	start = 0;
-	if (!(res = (char **)malloc(sizeof(char *) * (size + 1))))
+	if (!(res = (char **)malloc(sizeof(char *) * (ft_sizecount(s, c) + 1))))
 		return (NULL);
-	while (size > 0)
+	while (s[i] && s[i + 1])
 	{
-		if (s[i] == c && s[i] != c)
+		if (s[i] == c && s[i + 1] != c)
 			start = i + 1;
-		else
-			while (s[i++] != c && s[i + 1] == c)
-				size--;
-		res[j++] = ft_strsub(s, start, i - start);
+		if ((s[i] != c && s[i + 1] == c))
+			res[j++] = ft_strsub(s, start, i - start + 1);
+		if (i + 2 == ft_strlen(s) && s[i + 1] != c)
+			res[j++] = ft_strsub(s, start, i - start + 2);
 		i++;
-		size--;
 	}
+	res[j] = NULL;
 	return (res);
 }
-
-// static char	**make_array(char const *s, char c)
-// {
-// 	unsigned int	i;
-// 	int				len;
-// 	char			**res;
-
-// 	i = 0;
-// 	len = s[0] == c ? 0 : 1;
-// 	while (s[i] && s[i + 1])
-// 	{
-// 		if (s[i] == c && s[i + 1] != c)
-// 			len++;
-// 		i++;
-// 	}
-// 	if (!(res = (char **)malloc((len + 1) * sizeof(char *))))
-// 		return (NULL);
-// 	return (res);
-// }
-
-// char		**ft_strsplit(char const *s, char c)
-// {
-// 	unsigned int	i;
-// 	int				len;
-// 	int				start;
-// 	char			**res;
-
-// 	if (!s)
-// 		return (NULL);
-// 	if (!(res = make_array(s, c)))
-// 		return (NULL);
-// 	start = 0;
-// 	i = 0;
-// 	len = 0;
-// 	while (s[i] && s[i + 1])
-// 	{
-// 		if (s[i] == c && s[i + 1] != c)
-// 			start = i + 1;
-// 		if (s[i] != c && s[i + 1] == c)
-// 			res[len++] = ft_strsub(s, start, i - start + 1);
-// 		if (i + 2 == ft_strlen(s) && s[i + 1] != c)
-// 			res[len++] = ft_strsub(s, start, i - start + 2);
-// 		i++;
-// 	}
-// 	res[len] = ((void *)0);
-// 	return (res);
-// }
