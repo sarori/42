d3d365d08420adc19c911bba0cb6e4aabc10ace2
Saro Park <sapark@e1z2r6p8.42.us.org>
2019-05-17 17:43:10 -0700

    .

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,7 +6,7 @@
 /*   By: sapark <sapark@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/10 00:46:29 by sapark            #+#    #+#             */
-/*   Updated: 2019/05/17 17:41:26 by sapark           ###   ########.fr       */
+/*   Updated: 2019/05/17 17:43:06 by sapark           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 static char	**ft_sizecount(char const *s, char c)
 {
 	unsigned int	i;
-	int				cnt;
+	int				len;
 	char			**res;
 
 	i = 0;
-	if (s[0] != c)
-		cnt = 1;
-	else
-		cnt = 0;
+	len = s[0] == c ? 0 : 1;
 	while (s[i] && s[i + 1])
 	{
 		if (s[i] == c && s[i + 1] != c)
-			cnt++;
+			len++;
 		i++;
 	}
-	if (!(res = (char **)malloc((cnt + 1) * sizeof(char *))))
+	if (!(res = (char **)malloc((len + 1) * sizeof(char *))))
 		return (NULL);
 	return (res);
 }
